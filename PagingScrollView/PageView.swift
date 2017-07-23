d03fbe33835aefc95352016ea0911028cd1d5744
Josh Kuehn <joshmkuehn@gmail.com>
2017-07-23 13:47:09 -0500

    Finished making PageView and added placeHolderText to the main VC

--- PagingScrollView/PageView.swift
+++ PagingScrollView/PageView.swift
@@ -11,12 +11,83 @@ import UIKit
 
 class PageView: UIView {
   
+  private var headerTextField = UITextField()
+  var headerText: String = "" {
+    didSet {
+      headerTextField.text = headerText
+    }
+  }
+  
+  private var paragraphTextView = UITextView()
+  var paragraphText: String = "" {
+    didSet {
+      paragraphTextView.text = paragraphText
+    }
+  }
+  
+  init(frame: CGRect, headerText: String, paragraphText: String, backgroundColor: UIColor) {
+    super.init(frame: frame)
+    setup()
+    self.headerTextField.text = headerText
+    self.paragraphTextView.text = paragraphText
+    self.backgroundColor = backgroundColor
+    
+  }
+  
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
+    setup()
   }
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
+  func setup() {
+    
+    headerTextField.isUserInteractionEnabled = false
+    headerTextField.textColor = .black
+    headerTextField.textAlignment = .center
+    headerTextField.sizeToFit()
+    
+    paragraphTextView.isUserInteractionEnabled = false
+    paragraphTextView.textColor = .black
+    paragraphTextView.textAlignment = .center
+    paragraphTextView.sizeToFit()
+    paragraphTextView.isScrollEnabled = false
+    paragraphTextView.backgroundColor = .clear
+    
+    headerTextField.translatesAutoresizingMaskIntoConstraints = false
+    self.addSubview(headerTextField)
+    paragraphTextView.translatesAutoresizingMaskIntoConstraints = false
+    self.addSubview(paragraphTextView)
+    
+    NSLayoutConstraint.activate([
+      headerTextField.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+      headerTextField.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+      paragraphTextView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+      paragraphTextView.topAnchor.constraint(equalTo: headerTextField.bottomAnchor, constant: 20),
+      paragraphTextView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: (2/3))
+    ])
+  }
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- PagingScrollView/ViewController.swift
+++ PagingScrollView/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  let placeHolderText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
   override func viewDidLoad() {
     super.viewDidLoad()
