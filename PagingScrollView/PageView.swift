dbce12f387239941d5f81d0bf7692e6dd92ec17e
Josh Kuehn <joshmkuehn@gmail.com>
2017-07-23 13:04:20 -0500

    Created PageView and added required init methods

---
+++ PagingScrollView/PageView.swift
@@ -0,0 +1,22 @@
+//
+//  PageView.swift
+//  PagingScrollView
+//
+//  Created by JoshuaKuehn on 7/23/17.
+//  Copyright © 2017 Kuehn LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PageView: UIView {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
