98f2077ce0b6655b05d4859dd1b791625cc4a2ee
Josh Kuehn <joshmkuehn@gmail.com>
2017-07-23 19:09:02 -0500

    Added necessary code to display views in scrollView

--- PagingScrollView/ViewController.swift
+++ PagingScrollView/ViewController.swift
@@ -22,8 +22,33 @@ class ViewController: UIViewController {
   
   private func setup() {
     // *** SETUP SCROLLVIEW *** //
+    // [1]
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.backgroundColor = .black
+    scrollView.showsHorizontalScrollIndicator = false
+    
+    // [2]
+    self.view.addSubview(scrollView)
+    NSLayoutConstraint.activate([
+      scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+      scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+      scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+      scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+    ])
     
     // *** SETUP STACKVIEW AND ADD SUBVIEWS *** //
+    // [1]
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.distribution = .equalSpacing
+    
+    // [2]
+    scrollView.addSubview(stackView)
+    NSLayoutConstraint.activate([
+      stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+      stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+      stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+      stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor)
+    ])
     
     // Initializing the views we'll put in the scrollView and adding them to an array for convenience
     let pageView1 = PageView(headerText: "Header 1", paragraphText: placeHolderText, backgroundColor: .red)
@@ -34,6 +59,15 @@ class ViewController: UIViewController {
     views.append(pageView3)
     let pageView4 = PageView(headerText: "Header 4", paragraphText: placeHolderText, backgroundColor: .green)
     views.append(pageView4)
+    
+    // [1]
+    views.forEach { (view) in
+      view.translatesAutoresizingMaskIntoConstraints = false
+      // [2]
+      stackView.addArrangedSubview(view)
+      view.heightAnchor.constraint(equalTo: self.view.heightAnchor).isActive = true
+      view.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+    }
     
     // *** ADD PAGECONTROLL *** //
   }
